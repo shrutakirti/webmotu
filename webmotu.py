3fe7bbdc2f87769c64fcba7acc639386cc23b00e
shrutakirti <shrutakirti.gupta@gmail.com>
2015-07-02 14:49:36 +0100

    Update webmotu.py

--- webmotu.py
+++ webmotu.py
@@ -24,7 +24,8 @@ config = ConfigParser.ConfigParser()
 readconfig = config.read('/home/kirti/webmotu_config.cfg')#change to location of config file
 #app.config.from_envvar('readconfig', silent=True)
 
-bold_url = config.get('Section', 'bold_url')        #edit these bits 
+bold_url = config.get('Section', 'bold_url')
+bold_species_url = config.get('Section', 'bold_species_url')        #edit these bits 
 usrch = config.get('Section2','usearch_location')   #in config file
 kirti_desktop = tempfile.mkdtemp() 
 print kirti_desktop
@@ -110,13 +111,10 @@ def xml_parser(otu_xml):
         table = table + [[ID,taxonomic_id,similarity,url]]
         count = count + 1
        
-        if (count%10==0):
-            print "processed "+ str(count) +" sequences"
+    print "processed "+ str(count) +" matches"
            
     return table
 
-
-#boldresults.write('<html><head><title>WEBMOTU</title></head><body>')
 
 def dict_to_html(my_dict):
     boldresults = open('boldresults.html','w')
@@ -143,23 +141,23 @@ def get_bold_results():
     otus = fasta_read(kirti_desktop+'otus.fasta')
 
     my_dict = {}
-    keycount = 0
+    
     parsed_otu_table=[]
     parsed_otu_tables=[]
+    
     for otu in otus:
-        otu_xml = call_bold_api(otu)
+        otu_xml = call_bold_api(otu)#first call to BOLD
         parsed_otu_table = xml_parser(otu_xml)
         parsed_otu_tables.append(parsed_otu_table)
+#        specimen_data = specimen_data_retrieval(parsed_otu_table)#second call to BOLD
+#        info = specimen_data_parser(specimen_data)
 
         if my_dict.has_key(otu):
             my_dict.pop(otu)
             my_dict[otu] = parsed_otu_tables
         else:
             my_dict[otu] = parsed_otu_table
-       
-            
-    for key in my_dict.items():
-        keycount = keycount+1
+
     return my_dict
 
 #def stream_template(template_name, **context):
@@ -173,11 +171,101 @@ def get_bold_results():
 #    rows = flask.iter_all_rows()
 #    return Response(View.stream_template('index.html', rows=rows))
 
-     
+#SECOND CALL TO BOLD FOR DETAILED TAXONOMIC INFORMATION
+
+specimen_datas=[]
+def specimen_data_retrieval(table):
+    
+#    bold_species_url = http://www.boldsystems.org/index.php/API_Public/specimen?ids=
+    for row in table:
+        
+        print bold_species_url+str(row[0])
+        specimen_data = urllib.urlopen(bold_species_url+str(row[0]))
+        specimen_datas.append(specimen_data)
+    return specimen_datas
+
+
+def specimen_data_parser(specimen_datas):
+    
+    phylums = [] #list all phylums in result
+    class_names = []# list all classes in result
+    orders = []#list all orders in result
+    genuss = []#list all genuses in result
+    speciess = []#list all species in result
+    
+    phylum_set = set()#set of all phylums
+    classname_set = set()#set of all classes
+    order_set = set()#set of all orders
+    genus_set = set()#set of all genuses
+    species_set = set() #set of all species
+    
+    
+    for specimen_data in specimen_datas:
+        tree = ET.parse(specimen_data)
+        root = tree.getroot()
+    #parse data from second call to BOLD: detailed taxonomic information
+        for record in root.findall('record'):
+            
+            phylum = record.find('taxonomy').find('phylum').find('taxon').find('name').text
+            class_name = record.find('taxonomy').find('class').find('taxon').find('name').text
+            order = record.find('taxonomy').find('order').find('taxon').find('name').text
+            genus = record.find('taxonomy').find('genus').find('taxon').find('name').text
+            species = record.find('taxonomy').find('species').find('taxon').find('name').text
+            
+            phylums.append(phylum) #populate phylums[]
+            class_names.append(class_name)#populate classnames[]
+            orders.append(order) #populate orders[]
+            genuss.append(genus) #populate genuss[]
+            speciess.append(species) #populate speciess[]
+            
+            phylum_set.add(phylum) #populate phylum_set
+            classname_set.add(class_name) #populate classname_set
+            order_set.add(order) #populate order_set
+            genus_set.add(genus) #populate genus_set
+            species_set.add(species) #populate species_set
+
+    total_phylum_count = len(phylums) #total number of phylums in phylums[]
+    total_classname_count = len(class_names)#total number of classnames in class_names[]
+    total_order_count = len(orders)
+    total_genus_count = len(genuss)
+    total_species_count = len(speciess)
+    
+    for phylum in phylum_set:
+        phylum_count = phylums.count(phylum)#no of instances of a genus in genuss[]
+        phylum_percentage = float((phylum_count*100)/total_phylum_count)#percentage of a genus in genuss[]
+        print "no of "+phylum +" in list of phylums is "+ str(phylum_count)
+        print "% of "+phylum+ "is "+str(phylum_percentage)     
+    
+    for classname in classname_set:
+        classname_count = class_names.count(classname)#no of instances of a classname in class_names[]
+        classname_percentage = float((classname_count*100)/total_classname_count)#percentage of a classname in class_names[]
+        print "no of "+classname +" in list of classnames is "+ str(classname_count)
+        print "% of "+classname+ "is "+str(classname_percentage)
+
+    for order in order_set:
+        order_count = orders.count(order)#no of instances of a order in orders[]
+        order_percentage = float((order_count*100)/total_order_count)#percentage of a order in orders[]
+        print "no of "+order +" in list of orders is "+ str(order_count)
+        print "% of "+order+ "is "+str(order_percentage) 
+    
+    for genus in genus_set:
+        genus_count = genuss.count(genus)#no of instances of a genus in genuss[]
+        genus_percentage = float((genus_count*100)/total_genus_count)#percentage of a genus in genuss[]
+        print "no of "+genus +" in list of genuss is "+ str(genus_count)
+        print "% of "+genus+ "is "+str(genus_percentage) 
+    
+    for species in species_set:
+        species_count = speciess.count(species)#no of instances of a genus in genuss[]
+        species_percentage = float((species_count*100)/total_species_count)#percentage of a genus in genuss[]
+        print "no of "+species +" in list of speciess is "+ str(species_count)
+        print "% of "+species+ "is "+str(species_percentage)     
+
+
 class View(flask.views.MethodView):
           
     
     def get(self):
+        
         return flask.render_template('index.html')	
     
     def post(self):
@@ -196,14 +284,17 @@ class View(flask.views.MethodView):
             flask.flash('COMMUNICATING WITH BOLD...')
             my_dict = get_bold_results()
             boldresults = dict_to_html(my_dict)
+#            shutil.rmtree(kirti_desktop)
             os.system('cat '+'templates/init.html boldresults.html > templates/finalresults.html')
+#            specimen_data = specimen_data_retrieval(parsed_otu_table)
+#            info = specimen_data_parser(specimen_data)
 
             return flask.render_template('finalresults.html',boldresults = boldresults)
-            
+
           
         return self.get()
 
-    
+#    def post2(self):    
         
              
 app.add_url_rule('/', view_func=View.as_view('main'), methods = ['GET','POST'])
