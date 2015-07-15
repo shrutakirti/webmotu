e125a4680a7a6c11316252ad3cc8f187c8abc985
shrutakirti <shrutakirti.gupta@gmail.com>
2015-07-15 11:10:11 +0100

    <-- The app!

--- webmotu.py
+++ webmotu.py
@@ -8,16 +8,16 @@ s.bind(('127.0.1.1', 5000))
 import os
 import bold_helper
 import uparse_helper
-#import untitled7
+import untitled7
 import file_merge
 import flask, flask.views
-from flask import Response, request, stream_with_context
-import urllib
 import tempfile
 import shutil
+from flask import Flask, render_template, request, Response
+import time
 
-kirti_desktop = tempfile.mkdtemp() 
-print kirti_desktop
+tempdirectory = tempfile.mkdtemp() 
+print tempdirectory
 
 app = flask.Flask(__name__)
 app.secret_key = 'solong'
@@ -45,9 +45,13 @@ def validate(read_input, clustering_percentage):
     else:
         return False
 
-def dict_to_html(output_dict):
-    boldresult_html = kirti_desktop +'/boldresults.html'
+def dict_to_html(output_dict):#writes the first output html file
+    boldresult_html = 'boldresults.html'
     boldresults = open(boldresult_html,'w')
+    boldresults.write('<center><h3>PRELIMINARY TAXONOMIC INFORMATION: TOP HIT FOR EACH OTU</h3></center>')
+    boldresults.write('<center><form action"/result" method="View.post2"></h4>')
+    boldresults.write('<h4><a href="/result" class="mynewbutton">Get Detailed Taxonomic Information </a>')
+    boldresults.write('<center></form>')
     boldresults.write( '<table border = '+'"1" name = "boldresults" id = "boldresults">')
     boldresults.write('<thead><th><center>otu</center></th><th>Match Sequence ID</th><th>Taxonomic Identification</th><th>Similarity with Match</th><th>Match URL</th></thead>')
     for key in output_dict.keys():
@@ -63,6 +67,15 @@ def dict_to_html(output_dict):
     boldresults.write ('</table></body></html>')
     return boldresults
 
+#@app.route('/result')
+#def post2(output_dict):
+#        #BOLD CALLS FOR SPECIFIC TAXONOMIC INFORMATION
+#    specimen_data = untitled7.specimen_data_retrieval(output_dict)
+#    specimen_data_parsed = untitled7.specimen_data_parser(specimen_data)
+#    final = untitled7.write_to_file(specimen_data_parsed)
+#    file_merge.file_merge('templates/finaltable.html','outfile.html')
+#    return flask.render_template('finaltable.html',final=final)
+
 
 class View(flask.views.MethodView):
           
@@ -71,6 +84,7 @@ class View(flask.views.MethodView):
         
         return flask.render_template('index.html')	
     
+    @app.route('/taxonomy-info')
     def post(self):
         
         result = validate(flask.request.form['input_file_path'],flask.request.form['clustering_percentage'])
@@ -81,36 +95,42 @@ class View(flask.views.MethodView):
             return flask.render_template('index.html')
         else:
             #START UPARSE PIPELINE
-            os.system('cd '+kirti_desktop)
+            os.system('cd '+tempdirectory)
             path = flask.request.form['input_file_path']
             flask.flash('SUCCESSFULLY SUBMITTED '+path)
             
             flask.flash('CLUSTERING PERCENTAGE ACCEPTED')
+            
             try:
-                uparse_helper.uparse_pipeline(path,result,kirti_desktop)
+                uparse_helper.uparse_pipeline(path,result,tempdirectory)
             except OSError:
                 flask.flash("USEARCH NOT INSTALLED CORRECTLY")
                 return flask.render_template('index.html')                
+            
             flask.flash('UPARSE PIPELINE SUCCESSFULLY COMPLETED')
             flask.flash('COMMUNICATING WITH BOLD...')
             
             #UPARSE PIPELINE COMPLETE, START BOLD CALLS FOR GENERAL INFORMATION 
-            output_dict = bold_helper.get_bold_results(kirti_desktop)
-            boldresults = dict_to_html(output_dict[0])
-            file_merge.file_merge(kirti_desktop)
-
-            
+            output_dict = bold_helper.get_bold_results(tempdirectory)
+            boldresults = dict_to_html(output_dict)
+            file_merge.file_merge('templates/finalresults.html','boldresults.html')
+                        
             return flask.render_template('finalresults.html',boldresults = boldresults)
         return self.get()
+    
+    @app.route('/result')
+    def post2():
+            #BOLD CALLS FOR SPECIFIC TAXONOMIC INFORMATION
+        
+        specimen_data = untitled7.specimen_data_retrieval(output_dict)
+        specimen_data_parsed = untitled7.specimen_data_parser(specimen_data)
+        final = untitled7.write_to_file(specimen_data_parsed)
+        file_merge.file_merge('templates/finaltable.html','templates/outfile.html')
+        return flask.render_template('finaltable.html',final=final)
 
-    def post2(self):
-        #BOLD CALLS FOR SPECIFIC TAXONOMIC INFORMATION
-        my_dict = output_dict[1]
-        #specimen_data = untitled7.specimen_data_retrieval(my_dict)
-          
-        #info = untitled7.specimen_data_parser(specimen_data)
-        return flask.render_template('index.html')
+
+    
              
 app.add_url_rule('/', view_func=View.as_view('main'), methods = ['GET','POST','POST2'])
 app.debug = True
-app.run()
+app.run(threaded=True)
