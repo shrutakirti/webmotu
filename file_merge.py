55a98000a4ed642e1417e51fe4ba5c83f99c34db
shrutakirti <shrutakirti.gupta@gmail.com>
2015-08-08 13:55:39 +0100

    Delete file_merge.py

--- file_merge.py
+++
@@ -1,6 +0,0 @@
-import fileinput
-def file_merge(output_file,join_file):
-
-        with open(output_file,'w') as fout:
-                for line in fileinput.input(files=('templates/init.html',join_file)):
-                        fout.write(line)
