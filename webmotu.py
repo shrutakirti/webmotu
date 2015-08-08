a697a4a9eda3e60f3bade3b6cf838df0438e7048
shrutakirti <shrutakirti.gupta@gmail.com>
2015-08-08 13:55:20 +0100

    Update webmotu.py

--- webmotu.py
+++ webmotu.py
@@ -1,30 +1,29 @@
 # -*- coding: utf-8 -*-
 
-#import socket
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#s.bind(('127.0.1.1', 5000))
-
 import os
 import bold_helper
 import uparse_helper
 import untitled7
-import file_merge
 import flask, flask.views
 import tempfile
 import shutil
 from flask import Flask, render_template, request, Response
 import time
+from werkzeug import secure_filename
 
-tempdirectory = tempfile.mkdtemp() 
+UPLOAD_FOLDER = '/tmp/uploads'
+ALLOWED_EXTENSIONS = set(['fasta', 'fa','fas','fsa'])
+
+tempdirectory = tempfile.mkdtemp()
 print tempdirectory
 
 app = flask.Flask(__name__)
 app.secret_key = 'solong'
-
+app.config['UPLOAD_FOLDER'] = tempdirectory
 
 my_dict = {}
 output_dict ={}
+initial_fasta = ''
 
 def is_number(s):
     try:
@@ -33,9 +32,24 @@ def is_number(s):
     except ValueError:
         return False
 
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
 def validate(read_input, clustering_percentage):
-    if (read_input != ' ' and os.path.exists(read_input) and read_input.endswith('.fasta')):
-        
+    if flask.request.method == 'POST':
+        file = flask.request.files['browse']
+        if file and allowed_file(file.filename):
+            flask.flash("Uploading file")
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            read_input = app.config['UPLOAD_FOLDER'] + '/' + filename
+            initial_fasta = read_input
+        else:
+            flask.flash("INVALID INPUT FILE, EITHER NOT PROVIDED OR UNSUPPORTED EXTENSION")
+            return False
+    if (read_input != ' ' and os.path.exists(read_input)):
+
         if (clustering_percentage == '' or is_number(clustering_percentage)== False):
             clustering_percentage = 97.00
         clustering_percentage = float(clustering_percentage)
@@ -46,49 +60,40 @@ def validate(read_input, clustering_percentage):
         return False
 
 def dict_to_html(output_dict):#writes the first output html file
-    boldresult_html = 'boldresults.html'
-    boldresults = open(boldresult_html,'w')
-    boldresults.write('<center><h3>PRELIMINARY TAXONOMIC INFORMATION: TOP HIT FOR EACH OTU</h3></center>')
-    boldresults.write('<center><form action"/result" method="View.post2"></h4>')
-    boldresults.write('<h4><a href="/result" class="mynewbutton">Get Detailed Taxonomic Information </a>')
-    boldresults.write('<center></form>')
-    boldresults.write( '<table border = '+'"1" name = "boldresults" id = "boldresults">')
-    boldresults.write('<thead><th><center>otu</center></th><th>Match Sequence ID</th><th>Taxonomic Identification</th><th>Similarity with Match</th><th>Match URL</th></thead>')
+    open("templates/finalresults.html", "w").writelines(open("templates/init.html").readlines())
+    boldresult_html = 'templates/finalresults.html'
+    boldresults = open(boldresult_html,'a')
+    boldresults.write('<center><h3>PRELIMINARY TAXONOMIC INFORMATION: TOP HIT FOR EACH OTU</h3><p>Top hit = highest similarity + highest count</p></center>\n')
+    boldresults.write('<center><form action"/result" method="View.post2"></h4>\n')
+    boldresults.write('<h4><a href="/result" class="mynewbutton">Click to view Abundance & Detailed Taxonomic Information </a></h4>\n')
+    boldresults.write('<center></form>\n')
+    boldresults.write( '<table border = '+'"1" name = "boldresults" id = "boldresults" style="table-layout: fixed; width: 100%;text-overflow:ellipsis;overflow:hidden;white-space:nowrap;">\n')
+    boldresults.write('<thead><th><center>otu</center></th><th>Cluster Size</th><th>Match Sequence ID</th><th>Taxonomic Identification</th><th>Similarity with Match</th><th>Match URL</th></thead>')
     for key in output_dict.keys():
-        
         boldresults.write( '<tr>' + '<td>' + key + '</td>')
         for v in output_dict[key]:
-                   
+
             if v.startswith('http://'):
                 boldresults.write ('<td>'+'<a href = "'+str(v)+'" onclick=window.open(this.href) target=”_blank”>Click here for full record</a></td>')
             else:
                 boldresults.write ('<td>'+str(v)+'</td>')
-        boldresults.write('</tr>')
+        boldresults.write('</tr>\n')
     boldresults.write ('</table></body></html>')
+    boldresults.close()
     return boldresults
 
-#@app.route('/result')
-#def post2(output_dict):
-#        #BOLD CALLS FOR SPECIFIC TAXONOMIC INFORMATION
-#    specimen_data = untitled7.specimen_data_retrieval(output_dict)
-#    specimen_data_parsed = untitled7.specimen_data_parser(specimen_data)
-#    final = untitled7.write_to_file(specimen_data_parsed)
-#    file_merge.file_merge('templates/finaltable.html','outfile.html')
-#    return flask.render_template('finaltable.html',final=final)
-
-
 class View(flask.views.MethodView):
-          
-            
+
+
     def get(self):
-        
-        return flask.render_template('index.html')	
-    
+
+        return flask.render_template('index.html')
+
     @app.route('/taxonomy-info')
     def post(self):
-        
+
         result = validate(flask.request.form['input_file_path'],flask.request.form['clustering_percentage'])
-        
+
         if result == False:
             #TERMINATE
             flask.flash("INVALID INPUT")
@@ -96,44 +101,38 @@ class View(flask.views.MethodView):
         else:
             #START UPARSE PIPELINE
             os.system('cd '+tempdirectory)
-            path = flask.request.form['input_file_path']
-            flask.flash('SUCCESSFULLY SUBMITTED '+path)
-            
+            file = flask.request.files['browse']
+            filename = secure_filename(file.filename)
+            path = app.config['UPLOAD_FOLDER'] + '/' + filename
+            flask.flash('SUCCESSFULLY SUBMITTED FILE')
+
             flask.flash('CLUSTERING PERCENTAGE ACCEPTED')
-            
+
             try:
                 uparse_helper.uparse_pipeline(path,result,tempdirectory)
             except OSError:
                 flask.flash("USEARCH NOT INSTALLED CORRECTLY")
-                return flask.render_template('index.html')                
-            
+                return flask.render_template('index.html')
+
             flask.flash('UPARSE PIPELINE SUCCESSFULLY COMPLETED')
             flask.flash('COMMUNICATING WITH BOLD...')
-            
-            #UPARSE PIPELINE COMPLETE, START BOLD CALLS FOR GENERAL INFORMATION 
+
+            #UPARSE PIPELINE COMPLETE, START BOLD CALLS FOR GENERAL INFORMATION
             output_dict = bold_helper.get_bold_results(tempdirectory)
             boldresults = dict_to_html(output_dict)
-            file_merge.file_merge('templates/finalresults.html','boldresults.html')
             specimen_data = untitled7.specimen_data_retrieval(output_dict)
-            specimen_data_parsed = untitled7.specimen_data_parser(specimen_data)
+            specimen_data_parsed = untitled7.specimen_data_parser(specimen_data,output_dict)
             final = untitled7.write_to_file(specimen_data_parsed)
-            file_merge.file_merge('templates/finaltable.html','templates/outfile.html')           
+            open("templates/finaltable.html", "w").writelines(open("templates/init.html").readlines())
+            open("templates/finaltable.html", "a").writelines(open("templates/outfile.html").readlines())
+            os.system("rm -f webmotu/templates/outfile.html")
             return flask.render_template('finalresults.html')
         return self.get()
-    
+
     @app.route('/result')
     def post2():
-            #BOLD CALLS FOR SPECIFIC TAXONOMIC INFORMATION
-        
-#        specimen_data = untitled7.specimen_data_retrieval(output_dict)
-#        specimen_data_parsed = untitled7.specimen_data_parser(specimen_data)
-#        final = untitled7.write_to_file(specimen_data_parsed)
-#        file_merge.file_merge('templates/finaltable.html','templates/outfile.html')
         return flask.render_template('finaltable.html')
 
-
-    
-             
 app.add_url_rule('/', view_func=View.as_view('main'), methods = ['GET','POST','POST2'])
 app.debug = True
-app.run(host= '0.0.0.0')
+app.run(host= '0.0.0.0',threaded = True)
