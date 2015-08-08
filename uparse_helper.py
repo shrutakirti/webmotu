b93fcfae4679ec930344ef2666c700a25edbf7bd
shrutakirti <shrutakirti.gupta@gmail.com>
2015-08-08 13:53:51 +0100

    Update uparse_helper.py

--- uparse_helper.py
+++ uparse_helper.py
@@ -12,7 +12,6 @@ import subprocess
 
 config = ConfigParser.ConfigParser()
 readconfig = config.read('webmotu_usearch_config.cfg')#change to location of config file
-#app.config.from_envvar('readconfig', silent=True)
 
 usrch = config.get('Section','usearch_location')   #in config file
 
@@ -20,11 +19,11 @@ def uparse_pipeline(reads,clustering_percentage,kirti_desktop):
     clustering_percentage = clustering_percentage/100
     if "not found" in subprocess.check_output(usrch):
         raise OSError("usearch8 executable is not present in the path specified in the config, please double-check")
-    derep = usrch + ' -derep_fulllength ' + reads +  ' -fastaout '+kirti_desktop+'derep.fasta -sizeout'
-    ab_sort = usrch + ' -sortbysize '+ kirti_desktop+'derep.fasta -fastaout '+kirti_desktop+'sorted.fasta -minsize 2'
-    clustering_otus = usrch + ' -cluster_otus '+kirti_desktop+'sorted.fasta -otus '+kirti_desktop+'otus.fasta -relabel OTU_ -sizeout -uparseout '+kirti_desktop+'results.fasta'
-    mapping_reads = usrch + ' -usearch_global ' + reads + ' -db '+kirti_desktop+'otus.fasta -strand plus -id '+str(clustering_percentage)+' -uc '+kirti_desktop+'map.uc'
-    
+    derep = usrch + ' -derep_fulllength ' + reads +  ' -fastaout '+kirti_desktop+'/derep.fasta -sizeout'
+    ab_sort = usrch + ' -sortbysize '+ kirti_desktop+'/derep.fasta -fastaout '+kirti_desktop+'/sorted.fasta -minsize 2'
+    clustering_otus = usrch + ' -cluster_otus '+kirti_desktop+'/sorted.fasta -otus '+kirti_desktop+'/otus.fasta -relabel OTU_ -sizeout -uparseout '+kirti_desktop+'/results.fasta'
+    mapping_reads = usrch + ' -usearch_global ' + reads + ' -db '+kirti_desktop+'/otus.fasta -strand plus -id '+str(clustering_percentage)+' -uc '+kirti_desktop+'/map.uc'
+
     flask.flash('STARTING DEREPLICATION ...')
     os.system(derep)
     flask.flash('DEREPLICATION COMPLETE')
